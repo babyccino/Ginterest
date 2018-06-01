ab36035f36c7ac120b97e86d839e4df7c6af4eaa
Gus <gus.ryan163@gmail.com>
2018-06-02 00:58:21 +0800

    Remove test and old code

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -14,55 +14,20 @@ import { Post } from './../../core/models/post';
 	styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-	private _headerVisible: boolean = true;
 	private _isAuthenticated: boolean = true;
 	private _user: User;
 	private _userViewing: string = "/";
 
-  public signUpForm: FormGroup;
-
-	public get title()	{ return this.signUpForm.get('title'); 	}
-	public get url()		{ return this.signUpForm.get('url'); 		}
-	public get body()		{ return this.signUpForm.get('body'); 	}
-
 	public get isAuthenticated()	{ return this._isAuthenticated }
-	public get headerVisible()		{ return this._headerVisible; }
-	public get twitterUrl()				{ return `https://twitter.com/${this._user.twitter.username}`; }
 	public get user()							{ return this._user; }
 	public get userViewing()			{ return this._userViewing; }
 
 	constructor(
 		private router: Router,
-		private fb: FormBuilder,
 		private userService: UserService
 	) { }
 
-	@HostListener('window:resize', ['$event'])
-	onResize(event) {
-	  console.log('hey');
-	}
-
-	/*
-	@HostListener('window:scroll', ['$event']) scrollListener() {
-		console.log(this._scrollCount);
-
-		if (this._headerVisible)
-			if (this._scrollCount++ > 30) {
-				this.toggleHeader();
-				this._scrollCount = 0;
-			};
-	}
-	*/
-
 	ngOnInit() {
-		this.signUpForm  = this.fb.group({
-			title: ['', Validators.required],
-			url: ['',[
-				Validators.required,
-				Validators.pattern('^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$')
-			]],
-			body: ''
-		});
 		this.router.events.subscribe(event => {
 			if (event instanceof NavigationEnd) {
 				this._userViewing = event.url.slice(1);
@@ -72,14 +37,4 @@ export class HeaderComponent implements OnInit {
 		this.userService.isAuthenticated.subscribe(res => this._isAuthenticated = res)
 	}
 
-	public onFormSubmit(): void {
-		if (this.signUpForm.valid) {
-			let post:Post = this.signUpForm.value;
-			console.log(post);
-		}
-	}
-
-	public toggleHeader(): void {
-		this._headerVisible = !this._headerVisible;
-	}
 }
