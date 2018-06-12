39e99a3c9fca6a05ad09845f79923a3540ecc71b
Gus <gus.ryan163@gmail.com>
2018-06-12 18:13:46 +0800

    Add route change animation
    Board will swipe left when changing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,59 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+	transition,
+	trigger,
+	query,
+	style,
+	animate,
+	group,
+	animateChild, state
+} from '@angular/animations';
 import { UserService } from './core/services/user.service';
 import { PostService } from './core/services/post.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
+	animations: [
+		trigger('routerAnimation', [
+			transition('* <=> *', [
+    		query(':enter, :leave',
+    			style({ position: 'fixed', width:'100%' }),
+    			{ optional: true }
+    		),
+    		group([
+		      query(':enter', [
+		        style({ transform: 'translateX(100%)' }),
+		        animate('0.5s ease-in-out',
+		        	style({ transform: 'translateX(0%)' })
+		        )
+		      ], { optional: true }),
+		      query(':leave', [
+		        style({ transform: 'translateX(0%)' }),
+		        animate('0.5s ease-in-out',
+		        	style({ transform: 'translateX(-100%)' })
+		        )
+      		], { optional: true }),
+    		])
+			])
+		])
+	]
 })
 export class AppComponent implements OnInit {
-  title = 'app';
+	title = 'app';
 
-  constructor(
-  	private userService: UserService,
-    private postService: PostService
-  ) {}
+	constructor(
+		private userService: UserService,
+		private postService: PostService
+	) {}
 
-  ngOnInit() {
-  	this.userService.populate();
-  	this.postService.populate();
-  }
+	ngOnInit() {
+		this.userService.populate();
+	}
+
+	getPage(outlet) {
+		return outlet.activatedRouteData.state;
+	}
 
 }
